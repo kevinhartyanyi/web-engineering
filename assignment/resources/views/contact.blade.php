f7dd824420c86aad9cdcec13cbd8238f874a3cc4
Kevin A. Hartyányi <hartyanyi.kevin@gmail.com>
2021-03-28 17:23:48 +0200

    Added the contact page

---
+++ assignment/resources/views/contact.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.base')
+
+@section('content')
+<div class="card border-primary mb-3" style="margin-top: 30px;" >
+    <div class="card-header">Author</div>
+    <div class="card-body">
+      <h4 class="card-title">Kevin Attila Hartyányi</h4>
+    </div>
+  </div>
+  <div class="card border-primary mb-3" style="margin-top: 30px;" >
+    <div class="card-header">Email</div>
+    <div class="card-body">
+      <h4 class="card-title">c0s0rj@inf.elte.hu</h4>
+    </div>
+  </div>
+  <div class="card border-primary mb-3" style="margin-top: 30px;" >
+    <div class="card-header">Neptun code</div>
+    <div class="card-body">
+      <h4 class="card-title">C0S0RJ</h4>
+    </div>
+  </div>
+@endsection
+
---
+++ assignment/resources/views/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.base')
+
+@section('content')
+    <div class="jumbotron"  style="margin-top: 30px;">
+      <h1 class="display-4">LMS</h1>
+      <h3 class="lead">A learning management system.</h3>
+      <p> Here you can find all your learning needs in one place.</p>
+    </div>
+      <div style="width: 100%; display: table;">
+        <div style="display: table-row">
+            <div class="card bg-info mb-3" style="max-width: 20rem;display: table-cell;">
+                <div class="card-header">Student</div>
+                <div class="card-body">
+                  <h4 class="card-title">Student(Noun)</h4>
+                  <p class="card-text">
+                    A person who is studying at a school or other place of education.</p>
+                </div>
+              </div>
+              <div class="card bg-warning mb-3" style="max-width: 20rem;display: table-cell;">
+                <div class="card-header">Teacher</div>
+                <div class="card-body">
+                  <h4 class="card-title">Teacher(Noun) </h4>
+                  <p class="card-text">
+                    A person who teaches, especially in a school.</p>
+                </div>
+              </div>
+        </div>
+    </div>
+@endsection
+
